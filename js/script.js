04561bcb95298921239c44cdc36870f0c649bd8c
Richenda Gould <Richenda Gould>
2015-11-28 14:38:50 -0500

    Made carousel disappear and search results appear on click

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 $( document ).ready(function() {
 
+// Call date picker function for date selectors ===========================================
   $('.datepicker').pickadate({
 
   })
 
-  $("input").click(function(){
-      alert("The paragraph was clicked.");
+  $('#viewOnSearch').hide(); // hides search result panel
+
+// When Search button clicked ==============================================================
+  $(document).on('click', '#searchBtn', function () {
+    $("#hideOnSearch").addClass("hideOnSearch"); // Hides carousel by applying class
+    $('#viewOnSearch').show(); // Reveals search result panel
   });
 
 
-
+// End jQuery =============================================================================
 });
