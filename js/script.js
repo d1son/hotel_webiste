0d205b8b42a40fb6e5c1017bee8c97ea0e6f3c03
Richenda Gould <Richenda Gould>
2015-11-28 15:55:17 -0500

    Created a random listing on Search

--- js/script.js
+++ js/script.js
@@ -7,11 +7,34 @@ $( document ).ready(function() {
 
   $('#viewOnSearch').hide(); // hides search result panel
 
+  
+
+
 // When Search button clicked ==============================================================
   $(document).on('click', '#searchBtn', function () {
     $("#hideOnSearch").addClass("hideOnSearch"); // Hides carousel by applying class
     $('#viewOnSearch').show(); // Reveals search result panel
-  });
+
+    $("div.newListing").remove();
+
+      var hotelNames =["Extended Stay America", "La Quinta", "Hampton Inn", "Hilton Garden Inn", "Holiday Inn", "Comfort Inn", "EconoLodge", "Motel 6", "Bates Motel", "Ramada", "Days Inn", "Super 8"];
+      var randName = hotelNames[Math.floor(Math.random() * hotelNames.length)];
+
+      var hotelCities = ["New York", "Tulsa", "Sarasota", "Camden", "Sacramento", "Orlando", "Minneapolis", "Little Rock", "Austin", "Seattle", "New Orleans", "Chicago", "Charlotte"]; 
+      var randCity = hotelCities[Math.floor(Math.random() * hotelCities.length)];
+
+    $('<div>', { 
+          class: 'newListing panel-body'
+      }).append( $('<div>', { 
+          class: 'listName'
+      })).append( $('<div>', { 
+          class: 'listCity'
+      })).appendTo('#viewOnSearch');
+      $(".listCity").html(randCity);
+      $(".listName").html(randName);
+
+
+  }); // End Search button click ============
 
 
 // End jQuery =============================================================================
